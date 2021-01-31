662040efd904ee0a417a990cd1309ae11f924f05
mbeslic99 <73694599+mbeslic99@users.noreply.github.com>
2021-01-31 20:48:37 +0100

    Add files via upload

---
+++ CV/cv.php
@@ -0,0 +1,83 @@
+<?php
+	include("Static/header.php")
+?>
+<head>
+<title>Dobro došli u Jewelry Store</title>
+</head>
+      
+ <div class="home-welcome" id="Naslovna">
+            <div class="home-welcome-text" style="background-image: url(slike/slika.jpg); height:400px;  " >
+                <h1  >Dobrodošli u Jewelry Store</h1>
+                <h2  >Prodaja unikatnog i kvalitetnog nakita</h2>
+            </div>
+        </div>
+
+        <div class="onama" id="onama"></br></br></br></br@>
+       
+        <div class="row">
+              <div class = "column">
+                       <div class= "column-left">
+                           <img style =" width:450px; height: 400px;" src="slike/Logo.jpeg"></br></br></br></br></br></br>
+                          
+                        </div>
+                        
+                             
+                 </div>
+			<head>
+
+			</head>
+            <div class="row">
+            <div class="column-right">
+                <h3 style="text-align:center;" id="onama" >O nama</h3>
+                <p class="js"> Jewelry Store je adresa na kojoj možete pronaći unikatni nakit. </br>
+				Povijest kaže fini nakit ponekad,mi kažemo fini nakit za svaki dan. Naš nakit je ručno  izrađen poput starih 
+				vremena,ali dizajniran je za zlatne dane koji su pred nama.</br></br>
+				Kada je riječ o našoj proizvodnji, veterani smo i udružujemo se s vrhunskim draguljarima koji se obvezuju
+				na iznimnu izradu,etičke prakse i najfinije materijale.
+				Proizvodimo samo u malim količinama. Ne zato što volimo liste čekanja već zato što čvrsto vjerujemo u kvalitetu u odnosu na
+				količinu.</br></br>
+				Radimo samo s visokokvalitetnim postojanim materijalima, od plemenitih metala do lanaca postavljenih s 
+				pravim draguljima i dijamantima etičkog izvora.
+				Naši su komadi stvoreni za svakodnevno nošenje, zauvijek.</br></br>Pokretači projekta su studentice Matematika-Informatika.Kako su zamislile svoj projekt,koji im je cilj,motivacija te koje tehnologije će koristit možete pogledati <a href="https://drive.google.com/file/d/1Cbi4086ruMXm7IKbgwhM5Pi7u5aKBaSR/view?usp=sharing"
+  >ovdje</a></br></br>
+				Izaberi najfiniji nakit iz našeg asotrimana i pozlati svoju budućnost.</br></br></br></br></br></br>
+				
+              </p>
+				
+               </div>
+                        
+               </div>
+			   
+		</div> 
+<hr class="h">
+<div class="info">
+            <div class="info-text"  >
+                <h1 id="kontkat" >Kontakt</h1>
+                <h2 >Obratite nam se za sve bilo kakve nejasnoće</h2>
+            </div>
+        </div>
+<hr class="h">
+<div class="row" style="text-align:center; font-family: Times New Roman, Times, serif;">
+	<div class="col-md-4">
+		<img style=" height: 200px; width: 180px;" src="slike/Magdalena.jpg">
+		<p class="ime">Magdalena Bešlić</p>
+		<p> Rođena 19.07.1999.</br>SSŠ Posušje,smjer Ekonomija</br>2018. FPMOZ,smjer Matematika-Informatika</br>
+		<a href="mailto:magdalena.beslic@fpmoz.sum.ba">magdalena.beslic@fpmoz.sum.ba</a></p>
+	</div>
+	
+	<div class="col-md-4">
+		<img style=" height: 200px; width: 180px;" src="slike/Petrabrkic.jpg">
+		<p class="ime">Petra Brkić</p>
+		<p> Rođena 16.03.1997.</br>Gimnazija, Mostar</br>2015. FPMOZ,smjer Matematika-Informatika</br>
+		<a href="mailto:petra.brkic@fpmoz.sum.ba">petra.brkic@fpmoz.sum.ba</a></p>
+	</div>
+	
+	<div class="col-md-4">
+		<img style=" height: 200px; width: 180px;" src="slike/Petrabakula.jpg">
+		<p class="ime">Petra Bakula</p>
+		<p> Rođena 01.04.1998</br>Gimnazija fra Grge Martića, Posušje</br>2017. FPMOZ,smjer Matematika-Informatika</br>
+		<a href="mailto:petra.bakula2@fpmoz.sum.ba">petra.bakula2@fpmoz.sum.ba</a></p>
+	</div>
+</div>
+
+
---
+++ CV/static/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+		<script src="https://kit.fontawesome.com/cfb10f3ce9.js" crossorigin="anonymous"></script>
+	</head>
+
+<body>
+<nav class="navbar  sticky-top navbar-expand-lg navbar-light "style="background-color: #ecc6d9;">
+<a class="navbar-brand" href="#">
+    <img src="slike/Logo.jpeg" width="150" height="60" alt="" loading="lazy">
+  </a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="Naslovna.php"><i class="fas fa-home"></i>Naslovna <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#onama"><i class="fab fa-adn"></i>O nama</a>
+      </li>
+      
+      <li class="nav-item ">
+        <a class="nav-link " href="#kontkat" ><i class="far fa-envelope"></i>Kontakt</a>
+	  </li>
+	  <li class="nav-item">
+		<a class="nav-link" href="Proizvodi.php" ><i class="far fa-gem"></i>Proizvodi</a>
+	  </li>
+	  <li class="nav-item">
+        <a class="nav-link " href="Adminlogin.php" tabindex="-1" ><i class="fas fa-users-cog"></i>Admin</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+		<button class="btn btn-outline-secondary" type="button" ><a href="Login.php">Prijava</a></button>
+		<button class="btn btn-outline-secondary" type="button"><a href="Register.php">Registracija<a></button>
+	
+
+    </form>
+  </div>
+</nav>
+</body>
+</html>
